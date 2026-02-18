80154101fd7d87808edf2a9d7b3c182a1674b792
Lalegende21 <delfredtene17@gmail.com>
2026-02-18 17:58:50 +0100

    feat: add RefreshTokenRepository and VerificationTokenRepository

---
+++ src/main/java/com/example/demo/Repository/VerificationTokenRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.Repository;
+
+import com.example.demo.entites.User;
+import com.example.demo.entites.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+
+    Optional<VerificationToken> findByToken(String token);
+
+    Optional<VerificationToken> findByUserAndTokenTypeAndUsedFalse(
+            User user,
+            VerificationToken verificationToken
+    );
+
+    void deleteByUser(User user);
+}
